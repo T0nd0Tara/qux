f987b0878164923d35d1fc0140d2f1b6b9028ca4
T0nd0Tara <5632815@gmail.com>
2025-01-19 20:13:29 +0200

    add debug definition on debug compilation

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@ FLAGS="-Wextra -Wno-unknown-pragmas -std=c++20 -std=gnu++20"
 LIBS="-I /home/amirs/code-libs/magic_enum/include"
 FILES="./src/*.cpp"
 
+NUM_ARGS=$#
+ARGS=$@
+
+for arg in $ARGS; do
+  case $arg in 
+    'debug') FLAGS+=" -DDEBUG";;
+  esac
+done
+
 clang++ $FILES -o bin/qux $FLAGS $LIBS
