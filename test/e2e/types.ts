d16aac684c2d2f2a49589ec4536dc61ea0a9b23d
T0nd0Tara <5632815@gmail.com>
2025-09-19 15:49:13 +0300

    create a int type test and comment it out

--- test/e2e/types.ts
+++ test/e2e/types.ts
@@ -2,16 +2,17 @@ import { assertEquals } from "jsr:@std/assert";
 import { buildQuxProgram } from "../utils/build.ts";
 import type { CmdOutput } from "../types.ts";
 import { Suite, test } from "../utils/test.ts";
-const quxProgram = `
-main :: () {
-  return 1;
-};
-`;
 
 export default new Suite('types', 
   test(
     'func type',
     async () => {
+
+      const quxProgram = `
+main :: () {
+  return 1;
+};
+`;
       const args = new Set([
         '--no-write',
         '--stdin',
@@ -25,4 +26,26 @@ export default new Suite('types',
       assertEquals(quxRes.exitCode, 0);
     }
 
-  ));
+  ),
+//   test(
+//     'int type',
+//     async () => {
+//
+//       const quxProgram = `
+// a :: 4;
+// `;
+//       const args = new Set([
+//         '--no-write',
+//         '--stdin',
+//         '--print-types',
+//       ]);
+//
+//       const quxRes: CmdOutput = await buildQuxProgram(quxProgram, args);
+//
+//       assertEquals(quxRes.stdout, "a: int_ !> void_\n\n\n");
+//       assertEquals(quxRes.stderr, "");
+//       assertEquals(quxRes.exitCode, 0);
+//     }
+//
+//   ),
+);
